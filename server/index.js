3506169ac7b423f1a5c7af87c4106de060dfc02b
Jaron Hoste <jaron@hoste.me>
2022-06-14 10:37:01 +0200

    Removed token and updated to node v18

--- server/index.js
+++ server/index.js
@@ -2,21 +2,30 @@
 import fetch from 'node-fetch';
 import path from 'path';
 import express from 'express';
+import secrets from './secrets.json' assert {type: 'json'};
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const response = await fetch('https://dashboard.cphsense.com/api/v2/groups/136/latest', {
+const response = await fetch('https://nodejs.org/api/documentation.json', {
+// const response = await fetch('https://dashboard.cphsense.com/api/v2/groups/136/latest', {
         method: 'get',
         headers: {
         'Accept' : 'application/json',
-        'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTQ4NTI3NzMsIm5iZiI6MTY1NDg1Mjc3MywianRpIjoiYmQ5ZWQxNzUtYTA3My00MjY2LTljYmYtZjAyZjAyOTdhZDNhIiwiZXhwIjoxNjU0ODk1OTczLCJpZGVudGl0eSI6Imphcm9uQGhvc3RlLm1lIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.aGzLQFDvVxPaeBV9NHJ_r4jNTX-4GsYOOjctqe2UH64'
+        'Authorization' : `Bearer ` + secrets.API
         }
     });
 const data = await response.json();
 
 console.log(data);
+
+//todo hardcoded dummy data met apart id
+
+//todo binnenkomende data verwerken
+
+//todo schaal 1-10
 
 app.get("/api", (req, res) => {
     res.json(data);
